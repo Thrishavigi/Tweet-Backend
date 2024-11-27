59d9e164ebd7751cf7b80ebf596d3e433ceb7204
Thrishavigi <thrishavijayakumar7@gmail.com>
2024-11-27 16:18:08 +0530

    hcanges

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -26,7 +27,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
-
+app.use(cors());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
