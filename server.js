dca5131595411cd8481e88b18c5a2e32b237c23e
Thrishavigi <thrishavijayakumar7@gmail.com>
2024-11-27 16:28:20 +0530

    changes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
-app.use(cors());
+app.options("*", cors(corsOptions));  // Enable CORS for all OPTIONS requests
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
